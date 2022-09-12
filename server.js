c3d9eb2118fca3c81f6fb43d0bee4e4e2d32abcd
Stanislav Salnikov <delfi89@gmail.com>
2022-09-12 20:27:58 +0200

    Changed server.js settings. Added build step before the server starts

--- server.js
+++ server.js
@@ -3,15 +3,13 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const staticDir = `${__dirname}/static/`;
-app.use(express.static(staticDir));
-
 // Serve dist folder with routing
-// const distDir = `${__dirname}/dist/`;
-// app.use(express.static(distDir));
-// app.get('/*', function (req, res) {
-//   res.sendFile(`${distDir}/index.html`);
-// });
+const distDir = `${__dirname}/dist/`;
+app.use(express.static(distDir));
+
+app.get('/*', function (req, res) {
+  res.sendFile(`${distDir}/index.html`);
+});
 
 app.listen(PORT, function () {
   console.log(`Listening on port ${PORT}!`);
