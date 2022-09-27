ccb1c398c8aa94016bbb220c36e19dd94b10494c
Stanislav Salnikov <delfi89@gmail.com>
2022-09-27 20:09:27 +0200

    Fixed build

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ import { routeConsts, routes } from 'config/routes';
 import { BackButtonWrapper } from './components/BackButtonWrapper/BackButtonWrapper';
 import Button from './components/Button';
 import { CenteredBox } from './components/CenteredBox/CenteredBox';
-import Form from './components/Form';
 import FormGroup from './components/FormGroup';
 import { Input } from './components/Input/Input';
 import Link from './components/Link';
@@ -20,7 +19,6 @@ registerComponent(Input);
 registerComponent(ErrorComponent);
 registerComponent(FormGroup);
 registerComponent(Button);
-registerComponent(Form);
 registerComponent(CenteredBox);
 registerComponent(BackButtonWrapper);
 
