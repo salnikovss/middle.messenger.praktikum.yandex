44867946792b4f41fbd3984be675dc4754007a8d
Stanislav Salnikov <delfi89@gmail.com>
2022-10-14 10:20:42 +0200

    Removed unused files

--- src/config/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.webSocketHost = exports.apiHost = void 0;
-exports.apiHost = 'https://ya-praktikum.tech/';
-exports.webSocketHost = 'wss://ya-praktikum.tech/';
-//# sourceMappingURL=app.js.map
--- src/utils/apiHasError.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function apiHasError(response) {
-    return response && response.reason;
-}
-exports.default = apiHasError;
-//# sourceMappingURL=apiHasError.js.map
